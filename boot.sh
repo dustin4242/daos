9a7059b04859160edc39ffc92b6f4661c8e24102
dustin4242 <thegameexposer@gmail.com>
2023-10-24 11:56:54 -0500

    finally fixed issue with raw message in qemu

--- boot.sh
+++ boot.sh
@@ -1,2 +1,2 @@
 cargo bootimage --release
-qemu-system-x86_64 target/x86_64-options/release/bootimage-daos.bin
+qemu-system-x86_64 -drive format=raw,file=target/x86_64-options/release/bootimage-daos.bin
--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,21 @@
 #![no_main]
 
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
-    let hello = b"Let's All Love Lain";
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in hello.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0x0F;
-        }
-    }
-
+pub unsafe extern "C" fn _start() -> ! {
+    let lain = b"Let's All Love Lain";
+    print(0xb8000, lain);
     loop {}
 }
 
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
     loop {}
+}
+
+unsafe fn print(addr: usize, string: &[u8]) {
+    let vga_buffer = addr as *mut u8;
+    for (i, &byte) in string.iter().enumerate() {
+        *vga_buffer.offset(i as isize * 2) = byte;
+        *vga_buffer.offset(i as isize * 2 + 1) = 0x0F;
+    }
 }
